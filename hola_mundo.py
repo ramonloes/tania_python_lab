a3dc6443d8eafad7fa8f44171899b57884b818fc
Tania <a.klozevits@gmail.com>
2024-04-15 21:19:27 -0600

    new line

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,4 @@
 print("Hola mundo \nIniciamos un viaje en Python!" )
 print("Probando iniciar un pull request")
+print("\n")
+print("Se modifica el código, aún se me complica iniciar un pull request...")
